5403b7c5c1a60899a266fc3e54d356990031651a
Benjamin Marconnet <benjamin@marconnet.fr>
2016-03-29 19:50:03 +0200

    - loading icon - message "No Internet network available"

--- app/src/main/java/fr/marconnet/colisee/activities/MainActivity.java
+++ app/src/main/java/fr/marconnet/colisee/activities/MainActivity.java
@@ -4,14 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.ActivityOptions;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import fr.marconnet.acp.Config;
 import fr.marconnet.acp.models.Film;
@@ -25,12 +32,14 @@ public class MainActivity extends AppCompatActivity {
 
     private List<Film> films;
     private RecyclerView.Adapter adapter;
+    private ProgressBar loading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        loading = (ProgressBar) findViewById(R.id.loading);
         adapter = new FilmCardViewAdapter(new ArrayList<Film>());
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
@@ -38,7 +47,13 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
 
-        new DownloadFilmsTask().execute();
+        if (isNetworkAvailable()) {
+            loading.setVisibility(View.VISIBLE);
+            new DownloadFilmsTask().execute();
+        }
+        else {
+            setSnackbarNoInternet();
+        }
     }
 
     @Override
@@ -59,12 +74,33 @@ public class MainActivity extends AppCompatActivity {
                             ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(
                                     MainActivity.this, v, FilmActivity.INTENT_TRANSITION_ELEMENT);
                             startActivity(intent, options.toBundle());
-                        }
-                        else {
+                        } else {
                             startActivity(intent);
                         }
                     }
                 });
+    }
+
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+
+    private void setSnackbarNoInternet() {
+        CoordinatorLayout coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinator_layout);
+        Snackbar snackbar = Snackbar
+                .make(coordinatorLayout, "Pas de connexion Internet", Snackbar.LENGTH_INDEFINITE)
+                .setAction("REESSAYER", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        finish();
+                        startActivity(getIntent());
+                    }
+                });
+        snackbar.show();
     }
 
 
@@ -91,6 +127,7 @@ public class MainActivity extends AppCompatActivity {
                     ((FilmCardViewAdapter) adapter).addItem(result.get(i), i);
                 }
                 films = result;
+                loading.setVisibility(View.GONE);
             }
         }
 
